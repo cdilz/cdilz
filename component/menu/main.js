5264aaa7fe5574b92d7b1c0b90709f4e6b7bb9b9
cdilz <49366094+cdilz@users.noreply.github.com>
2021-01-11 13:42:09 -0500

    Navigation creation.

--- component/menu/main.js
+++ component/menu/main.js
@@ -21,17 +21,35 @@ function entriesToList(entries)
 
 export default function menu()
 {
-	return (
-		<nav className={style.menuButton + ' material-icons'}>menu</nav>
-	)
-	/*
 	let entries =
 	[
 		 {text: 'Home', link: '/'}
-		,{text: 'Test', link: '/'}
 	]
-
 	let list = entriesToList(entries)
+	
+	return (
+		<nav className={style.menuContainer}>
+			<nav className={style.menuButton + ' material-icons mainMenuButton'}>menu</nav>
+			<nav className={style.fullscreenMenuContainer + ' mainMenuContainer'}>
+				<nav className={style.menuBox}>
+					<h1 className={style.menuHeader}>
+						<Link href='/'>
+							<a>
+								CDilz
+							</a>
+						</Link>
+					</h1>
+					<ol className={style.menuList}>
+						{list}
+					</ol>
+				</nav>
+			</nav>
+			<script type='text/javascript' src='/script/menu/main.js'></script>
+		</nav>
+		
+	)
+	/*
+
 
 	return (
 		<nav className={styles.menu}>
