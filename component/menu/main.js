8620b6ce6bcb8683ad7f1fc00813ef5253225b76
cdilz <49366094+cdilz@users.noreply.github.com>
2021-01-11 15:21:31 -0500

    Fix click handler not staying on page transition.

--- component/menu/main.js
+++ component/menu/main.js
@@ -1,15 +1,39 @@
 import style from './menu.main.module.css'
 import Link from 'next/link'
 
+function getMainMenuContainer()
+{
+	return document.querySelectorAll('.mainMenuContainer')[0]
+}
+
+function confirmAndHideClickMenuContainer(e)
+{
+	if(e.target.classList.contains('mainMenuContainer'))
+	{
+		hideMenu()
+	}
+}
+
+function displayMenu()
+{
+	getMainMenuContainer().style.display = 'flex'
+}
+
+function hideMenu()
+{
+	getMainMenuContainer().style.display = 'none'
+}
+
+
 function entriesToList(entries)
 {
 	let output = []
 	for(let i = 0; i < entries.length; i++)
 	{
 		output.push(
-		<li className={style.menuEntry} key = {i}>
+		<li className={style.menuEntry} key = {i} onClick={hideMenu}>
 			<Link href={entries[i].link}>
-				<a>
+				<a className='mainMenuLink'>
 					{entries[i].text}
 				</a>
 			</Link>
@@ -23,16 +47,17 @@ export default function menu()
 {
 	let entries =
 	[
-		 {text: 'Home', link: '/'}
+		 {text: 'Home', link: '/'},
+		 {text: 'Test', link: '/test'}
 	]
 	let list = entriesToList(entries)
 	
 	return (
 		<nav className={style.menuContainer}>
-			<nav className={style.menuButton + ' material-icons mainMenuButton'}>menu</nav>
-			<nav className={style.fullscreenMenuContainer + ' mainMenuContainer'}>
+			<nav className={style.menuButton + ' material-icons mainMenuButton'} onClick={displayMenu}>menu</nav>
+			<nav className={style.fullscreenMenuContainer + ' mainMenuContainer'} onClick={confirmAndHideClickMenuContainer}>
 				<nav className={style.menuBox}>
-					<h1 className={style.menuHeader}>
+					<h1 className={style.menuHeader + ' mainMenuLink'} onClick={hideMenu}>
 						<Link href='/'>
 							<a>
 								CDilz
@@ -44,19 +69,7 @@ export default function menu()
 					</ol>
 				</nav>
 			</nav>
-			<script type='text/javascript' src='/script/menu/main.js'></script>
 		</nav>
 		
 	)
-	/*
-
-
-	return (
-		<nav className={styles.menu}>
-			<ol className={styles.menuList}>
-				{list}
-			</ol>
-		</nav>
-	)
-	*/
 }
